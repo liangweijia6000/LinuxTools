40a75eeb03983f4240cf96bd928d28792cb833a5
liangweijia <452434058@qq.com>
2020-02-29 00:33:56 +0800

    mount

---
+++ mountWin.sh
@@ -0,0 +1,4 @@
+sudo mount -t cifs -o username=admin,password=123456 //10.10.1.220/g ~/shareG
+sudo mount -t cifs -o username=admin,password=123456 //10.10.1.220/e ~/shareE
+sudo mount -t cifs -o username=admin,password=123456 //10.10.1.220/h ~/shareH
+sudo mount -t cifs -o username=admin,password=123456 //10.10.1.220/d ~/shareD
